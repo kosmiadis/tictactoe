ad404501af4b5e7872cfe15302736b1bd586040a
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2022-06-30 14:21:20 +0300

    Update

--- starting-menu.js
+++ starting-menu.js
@@ -1,7 +1,18 @@
 const screens = []
+const btnSound = new Audio('audio/btn-sound1.mp3')
 
 const startingMenu = document.querySelector('.starting-menu')
 const OneVSOneGameScreen = document.querySelector('.oneVSone-game-screen')
+const optionsBtn = document.querySelector('#options')
+const buttons = document.querySelectorAll('button')
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        btnSound.play()
+    })
+})
+optionsBtn.addEventListener('click', e => {
+    
+})
 screens.push(startingMenu)
 screens.push(OneVSOneGameScreen)
 
