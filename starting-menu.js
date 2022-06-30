c67727f473ee5783701d58a444cd53fc7cefbbb7
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2022-06-30 13:48:49 +0300

    Update

---
+++ starting-menu.js
@@ -0,0 +1,10 @@
+const screens = []
+
+const startingMenu = document.querySelector('.starting-menu')
+const OneVSOneGameScreen = document.querySelector('.oneVSone-game-screen')
+screens.push(startingMenu)
+screens.push(OneVSOneGameScreen)
+
+//screens.forEach(screen => console.log(screen))
+
+
