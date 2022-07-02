b08aeebc31491c0612ee9f60619a03326fb2d451
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2022-07-02 13:22:05 +0300

    adding functionality

--- main-game.js
+++ main-game.js
@@ -2,7 +2,7 @@ const player = document.querySelector('#player')
 const playerSpan = player.querySelector('span')
 const boxes = document.querySelectorAll('.box')
 let startingPlayer = 'X'
-
+let boxesFull = 0
 
 play1V1Btn.addEventListener('click', e => {
     carouselContainer.style.display = 'none'
@@ -20,8 +20,10 @@ function startGame() {
     boxes.forEach(box => {
         box.addEventListener('click', e => {
             e.target.textContent = startingPlayer
+            checkIfWin()
             changePlayerOrder()
             playerSpan.textContent = startingPlayer
+            boxesFull++
             
         })
         
@@ -41,8 +43,47 @@ function changePlayerOrder () {
 }
 
 function checkIfWin () {
+
+    for (let i=0; i<=6; i++) {
+        if (i==0) {
+            checkHorizontal(i,startingPlayer)
+        }
+
+        if (i==3) {
+            checkHorizontal(i,startingPlayer)
+        }
+        if (i==6) {
+            checkHorizontal(i,startingPlayer)
+        }
+    }
+
+    if (boxesFull == 8) {
+        playerSpan.textContent = 'None Won!'
+        returnToMenu()
+    }
     
 }
+
+function checkHorizontal(i ,currentPlayer) {
+    if (boxes[i].textContent == currentPlayer & boxes[i+1].textContent == currentPlayer & boxes[i+2].textContent == currentPlayer) {
+        playerSpan.textContent = startingPlayer + ' ' + 'Won!'
+        returnToMenu()
+        
+    }
+    
+}
+
+
+function returnToMenu () {
+    
+    setTimeout(function returnToMenu () {
+        carouselContainer.style.display = 'block'
+        OneVSOneGameScreen.style.display = 'none'
+        boxes.forEach(box => box.textContent = '')
+    }, 2000)
+
+}
+
 
 //for stoping the screen moving when touchmove event occurs in mobile
 document.addEventListener("touchmove", function (e) {e.preventDefault()}, {passive:false}
